e7e26b78792189f33fab6be73df68e7f687c2ba6
Saurabh Gajjar <saurabhgajjar08@gmail.com>
2022-10-04 11:34:31 +0000

    Update 7 files

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,7 @@
 import { StrictMode } from "react";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import * as ReactDOMClient from "react-dom/client";
 
 import App from "./App";
